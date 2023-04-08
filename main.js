24848b1f211efd0b982d49a2f250e86e43794f6a
animemuically6@gamil.com <animemuically6@gamil.com>
2023-04-08 22:01:45 +0530

    wed side aswini kumar

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+
+console.log('Hello World!');
+var sidemeu = document.getElementById('sidemenu');
+  
+  function openemenu(){
+    sidemenu.style.right ='-100%';
+  }
+  
+  function closemenu(){
+        sidemenu.style.right ='0px';
+
+  }
+
+/*------------------------slider js----*/
+
