dade4244c94b8d7d8397bc7eaedcd3c284362338
mohitsuper <animemusically6@gmail.com>
2023-08-11 18:15:48 +0530

    web

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ console.log('Hello World!');
 var sidemeu = document.getElementById('sidemenu');
   
   function openemenu(){
-    sidemenu.style.right ='-100%';
+    sidemenu.style.right ='0%';
   }
   
   function closemenu(){
-        sidemenu.style.right ='0px';
+        sidemenu.style.right='100%';
+        
 
   }
   
